b495ae7d50bd196dbb95cab4c1749e988657c630
anufreichik <marta149@hotmail.com>
2021-10-17 09:20:08 -0700

    OrderEntry test for alert added

--- src/pages/entry/Options.tsx
+++ src/pages/entry/Options.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import {Row} from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 
 export interface OptionsProps{
     optionType:string;
@@ -13,15 +14,22 @@ export interface IItem{
 }
 const Options:React.FC<OptionsProps>=({optionType})=> {
 
-    const [items, setItems]=useState<IItem[]>([])
+    const [items, setItems]=useState<IItem[]>([]);
+    const [error, setError] = useState(false);
+
     //optionType is 'scoops' or 'toppings'
     useEffect(() => {
        axios.get(`http://localhost:3030/${optionType}`)
            .then(response=>setItems(response.data))
-           .catch(error=>console.log(error))
+           .catch(error=>{
+               setError(true);
+           })
     }, [optionType]);
 
     //
+    if(error){
+        return <AlertBanner message={'An unexpected error occurred. Please try again later.'} variant={'danger'}/>;
+    }
     const ItemComponent = optionType==='scoops'? ScoopOption : ToppingOption;
 
     const optionItems = items.map(item=>(<ItemComponent key={item.name}
